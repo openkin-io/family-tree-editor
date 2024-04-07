68812a667af6146fc86e005dca9c2958e4dfd9de
Nicolas Epstein <nilueps@gmail.com>
2024-04-07 11:31:46 -0400

    Initial commit

---
+++ src/lib/family-tree.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @class
+ * @classdesc The actual family tree app shown to the user.
+ */
+export class FamilyTree {
+	constructor(tree, config) {
+		this.tree = null;
+		document.getElementById("app").innerHTML = "Hello World";
+	}
+}
